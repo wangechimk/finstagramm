23d4b418e6e7c4342357bd66ebeae1b4afd0b942
wangechimk <wangechik.michelle@gmail.com>
2021-05-21 02:50:43 +0300

    make changes to the templates

--- project_insta/settings.py
+++ project_insta/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_insta'
+    'app_insta',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
