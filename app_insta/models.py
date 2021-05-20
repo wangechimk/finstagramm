51749ebe1e5d9a9c920cc29afe57a92548643db5
wangechimk <wangechik.michelle@gmail.com>
2021-05-20 21:24:58 +0300

    create image and profile models with migrations

--- app_insta/models.py
+++ app_insta/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Images(models.Model):
+    image = models.ImageField(upload_to='images/')
+    image_name = models.CharField(max_length=30)
+    caption = models.TextField()
+    profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
+
+
+
+class Profile(models.Model):
+    photo = models.ImageField(upload_to='avatars/')
+    bio = models.TextField() 
--- app_insta/urls.py
+++ app_insta/urls.py
@@ -1,7 +1,12 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
+app_name='app_insta'
 
 urlpatterns=[
     path('', views.feed, name='feed'),
 ] 
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_URL) 
