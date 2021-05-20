7cfb028974cd1e0395714e0e4c7ef82e0951ecf7
wangechimk <wangechik.michelle@gmail.com>
2021-05-20 23:17:05 +0300

    alter bio field in profile model

--- app_insta/models.py
+++ app_insta/models.py
@@ -9,7 +9,6 @@ import datetime
 class Images(models.Model):
     ''' a model for Image posts '''
     image = models.ImageField(upload_to='images/')
-    image_name = models.CharField(max_length=30)
     caption = models.TextField()
     profile = models.ForeignKey('Profile', on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, blank=True)
@@ -19,14 +18,54 @@ class Images(models.Model):
 class Meta:
     ordering = ['?'] #order randomly on feed
 
+    def save_image(self):
+        ''' method to save an image post instance '''
+        self.save()
+
+    def delete_image(self):
+        '''method to delete an image post instance '''
+        self.delete()
+
+    def update_caption(self, new_caption):
+        ''' method to update an image's caption '''
+        self.caption = new_caption
+        self.save()
+
+    @classmethod    
+    def get_user_images(cls, user_id):
+        ''' method to retrieve all images'''
+        img = Images.objects.filter(profile=user_id).all()
+        sort = sorted(img, key=lambda t: t.created_on)
+        return sort
+
 class Profile(models.Model):
-    ''' a User model extended'''
+    ''' extended User model'''
     user = models.OneToOneField(User, on_delete=models.CASCADE,default=1)
     photo = models.ImageField(default='default.jpg', upload_to='avatars/')
     bio = models.TextField(max_length=500, blank=True, default=f'Hi, my name is {User.username}')
 
     def __str__(self):
         return f'user {self.user.username}'
+    
+    def save_image(self):
+        ''' method to save a user's profile '''
+        self.save()
+
+    def delete_profile(self):
+        '''method to delete a user's profile '''
+        self.delete()
+
+    def update_bio(self, user_id, new_bio):
+        ''' method to update a users profile bio '''
+        user = User.objects.get(id=user_id)
+        self.bio = new_bio
+        self.save()
+
+    def update_image(self, user_id, new_image):
+        ''' method to update a users profile image '''
+        user = User.objects.get(id=user_id)
+        self.photo = new_image
+        self.save()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -48,6 +87,20 @@ class Comments(models.Model):
 
     class Meta:
         ordering = ['created_on']
+    
+    def save_comments(self):
+        ''' method to save comment instance '''
+        self.save()
+
+    def delete_comment(self):
+        '''method to delete comment instance '''
+        self.delete()
+
+    def edit_comment(self, new_comment):
+        ''' method to edit a comment '''
+        self.comment_body = new_comment
+        self.save()
+
 
     def __str__(self):
         return 'Comment by {self.name}'
@@ -55,6 +108,7 @@ class Comments(models.Model):
 
 
 class Relations(models.Model):
+    ''' model for user relations: follower-following system'''
     follower = models.ForeignKey('Profile', related_name='following', on_delete=models.CASCADE)
     followed = models.ForeignKey('Profile', related_name='followers', on_delete=models.CASCADE)
 
--- app_insta/tests.py
+++ app_insta/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Images, Profile, Comments, Relations
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestImages(TestCase):
+    ''' test class for images model '''
+    def setUp(self):
+        ''' method to create Image instances to be called before each test case'''
+        self.test_user = User(username='Linda', password='123')
+        self.test_user.save()
+        self.test_profile = Profile(user=self.test_user, photo='avatars/anime.jpg')
+
+        self.test_comment = Comments(name=self.test_profile, comment_body='Test comment', created_on=datetime.now())
+
+        self.test_image = Images(image='images/test.jpg', caption='some text', profile=self.test_profile, comments=self.test_comment, created_on=datetime.now())
+
+    def test_instance(self):
+        ''' '''
+        self.assertTrue(isinstance(self.test_image, Images))
+
+    def tearDown(self):
+        ''' '''
+        self.test_user.delete()
+        Profile.objects.all().delete()
+        Comments.objects.all().delete()
+        Images.objects.all().delete() 
