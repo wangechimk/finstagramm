87a92a8036a33ca5a18af86c5f643d7f25689688
wangechimk <wangechik.michelle@gmail.com>
2021-05-21 23:30:21 +0300

    add django registration

--- app_insta/views.py
+++ app_insta/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Image, Profile, Comment, Relation
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def feed(request):
-    return render(request, 'index.html')
+    pictures = Image.objects.all()
+    return render(request, 'index.html',{'pictures':pictures})
 
+@login_required(login_url='/accounts/login/')
 def profile(request):
     return render(request, 'profile.html') 
    
